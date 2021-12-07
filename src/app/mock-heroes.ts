bcce4d3469c88175be3416932150267b86ba7b44
Fernando Gekdyszman <fergek@Fernandos-MacBook-Pro.local>
2021-12-07 11:40:02 -0300

    display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[] = [
+    {id: 11, name: "Dr. Nice"},
+    {id: 12, name: "Narco"},
+    {id: 13, name: "Bombasto"},
+    {id: 14, name: "Celeritas"},
+    {id: 15, name: "Magneta"},
+    {id: 16, name: "Agapornis"},
+    {id: 17, name: "Mateos"},
+    {id: 18, name: "Miranda"},
+    {id: 19, name: "Dividido"},
+    {id: 20, name: "Albuquerque"},
+];
